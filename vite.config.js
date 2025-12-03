4a1173624cc7b9fa102d1746b13327b8b808ee15
ZeromaPremium <wakil1amin2007@gmail.com>
2025-12-03 14:55:42 +0600

    Fix white page with relative base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // REPLACE 'gscpc-web' WITH YOUR EXACT GITHUB REPOSITORY NAME
-  base: '/GSCPC/', 
+  base: './', // <--- CHANGE THIS (Use dot slash)
 })
